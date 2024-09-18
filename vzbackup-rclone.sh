57aef76b3a752d771b852cbbb148523819794301
Dylan Gerow <dmgerow@gmail.com>
2024-09-18 14:49:22 -0500

    fix

--- vzbackup-rclone.sh
+++ vzbackup-rclone.sh
@@ -3,7 +3,7 @@
 
 ############ /START CONFIG
 dumpdir="/var/lib/vz/dump" # Set this to where your vzdump files are stored
-remoteroot="Proxmox\ Backups" # Set this to the remote backup directory
+remoteroot="Proxmox Backups" # Set this to the remote backup directory
 MAX_AGE=3 # This is the age in days to keep local backup copies. Local backups older than this are deleted.
 MAX_CLOUD_AGE=10 # This is the age in days to keep cloud backup copies. Cloud backups older than this are deleted
 ############ /END CONFIG
