7fdb3b0f92a9d2130db43838c7a1974e1d28dc1c
Dylan Gerow <dmgerow@gmail.com>
2024-09-22 15:51:52 -0500

    change to 7 days of backups

--- vzbackup-rclone.sh
+++ vzbackup-rclone.sh
@@ -6,7 +6,7 @@ drive="gd-backup_crypt"
 dumpdir="/var/lib/vz/dump" # Set this to where your vzdump files are stored
 remoteroot="Proxmox Backups" # Set this to the remote backup directory
 MAX_AGE=3 # This is the age in days to keep local backup copies. Local backups older than this are deleted.
-MAX_CLOUD_AGE=10 # This is the age in days to keep cloud backup copies. Cloud backups older than this are deleted
+MAX_CLOUD_AGE=7 # This is the age in days to keep cloud backup copies. Cloud backups older than this are deleted
 ############ /END CONFIG
 
 _bdir="$dumpdir"
